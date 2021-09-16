186c7cd1745b586cf14aef4c9282c6ac16800b26
guanli <guanli@tencent.com>
2021-09-16 21:11:57 +0800

    fix: baidu

---
+++ 面试/百度游戏部门二面.js
@@ -0,0 +1,19 @@
+// 项目
+// 项目难点
+
+// axios原理
+
+// webpack
+// webpack打包原理
+// 常见loader，cssloader、styleloader
+// 如何手写loader
+// babel原理
+// 配置ts
+
+// 浏览器缓存机制
+
+// vue react循环渲染的中的key
+
+// js垃圾回收
+
+// 性能优化
---
+++ 面试/知乎搜索前端二面.js
@@ -0,0 +1,13 @@
+
+// 函数式组件、类组件区别
+// 如何在类组件使用hooK
+// redux 数据状态变化、如何做到页面渲染
+
+
+// aios如何在node和浏览器同时运行
+// axios如何把xml callback形式封装成promise链式调用形式
+// xml状态码，如果没有状态码怎么办
+// axios timeout如何实现
+
+// 如何用promise api实现有超时限制的allseteled
+// 如何用promise.all实现allSeteled

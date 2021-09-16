cc7d1b88d1c5bbcd8f868c052fb6e55ae435a2be
guanli <guanli@tencent.com>
2021-09-16 18:25:17 +0800

    fix: 京东

---
+++ 面试/京东零售机票二面.js
@@ -0,0 +1,11 @@
+// react element 与 react component
+// react hook（useRef useReducer useLayoutEffect）
+// forwordRef 
+// react优先级
+// react Fibber
+// react render之后的环状链表
+
+// js compose pipe
+// js 柯里化
+
+// 开发流程是怎样的

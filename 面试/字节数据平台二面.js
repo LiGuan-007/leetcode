4d2ce0b97eccda915598a3cee379818f9a41c3fb
guanli <guanli@tencent.com>
2021-09-22 10:01:53 +0800

    add

---
+++ 面试/字节数据平台二面.js
@@ -0,0 +1,35 @@
+// ts
+// ReturnType
+// Partical
+// 新项目如何接入
+// 现有项目如何迁移ts
+
+// redux
+// redux如何和react绑定
+// connet原理
+// 只改c，a会变化吗？
+// {
+//   a: {
+//       b: { c: 1 }
+//   }
+//   }
+
+
+// react
+// 各个hook是在组件渲染各个时机中如何处理的
+// useEffect  uselayoutEffect时机
+
+// 有问题吗
+// useEffect(async () => {
+//   const user = await service.getuser();
+//   setState(......)
+// }, []);
+
+// 实现Select组件，接口请求拉1000w条数据如何实现
+// 多个这种Select组合在一起使用，有问题吗
+// 网络请求如何终止
+
+
+// 找到一个n维数组中出现次数最多的数字们
+// [1,2,3,4,5,[1,2,3,[1,2,1,[1,2,2]]]]
+// =>[1, 2]
